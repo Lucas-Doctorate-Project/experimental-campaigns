c80b7ad35323429026be6696b2e730c6b99ef06f
André Schwarz <aschwarz@usp.br>
2026-06-09 00:32:15 -0300

    Improved bash script somewhat

--- bashImplementation.sh
+++ bashImplementation.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+trap "kill 0" SIGINT
 
 # === CONFIGURATION PARAMETERS ===
-NPROC=$(($(nproc) / 2))
+NPROC=$(($(nproc)))
 if [ "${NPROC}" -le 0 ]; then
     NPROC=1
 fi
@@ -16,10 +17,17 @@ fi
 
 TOML_FILE=${1}
 
+if [ -f ${TOML_FILE} ]; then
+	echo "Parsing experiments data from ${TOML_FILE}..."
+else
+	echo "ERROR: Experiments file ${TOML_FILE} not found."
+	echo "Terminating bash runner."
+	exit 1
+fi
+
 mkdir -p "${OUTPUT_DIR}"
 
 # === MAIN LOOP ===
-echo "Parsing experiments data from ${TOML_FILE}..."
 ITERATOR=0
 
 # Reads the output of parse_toml.py line by line
@@ -33,7 +41,7 @@ while IFS=$'\t' read -r NAME VARIANT PLATFORM WORKLOAD TRACE VOPTS; do
     
     for FILENAME in ${FULL_PLATFORM} ${FULL_WORKLOAD} ${FULL_TRACE} ${FULL_VOPTS}; do
         if [ ! -f "${FILENAME}" ]; then
-            echo "[${NAME}] WARNING: file ${FILENAME} not found. Skipping."
+            echo "[${NAME}] WARNING: file ${FILENAME} not found. Skipping experiment."
             continue
         fi
     done
@@ -49,22 +57,24 @@ while IFS=$'\t' read -r NAME VARIANT PLATFORM WORKLOAD TRACE VOPTS; do
     echo "[${NAME}] Variant: ${VARIANT} | Platform: ${PLATFORM} | Workload: ${WORKLOAD} | Trace: ${TRACE} | Vopts: ${VOPTS}"
     
     (
-        batsim -p "${FULL_PLATFORM}" \
+	timeout 300 batsim -p "${FULL_PLATFORM}" \
                -w "${FULL_WORKLOAD}" \
                -e "${OUTPUT_DIR}/batsim_${NAME}" \
                --socket-endpoint "ipc://socket_${NAME}" \
                --energy \
                --environmental-footprint-dynamic "${FULL_TRACE}" &> /dev/null &&
-	       echo "[${ITERATOR}] Batsim finished successfuly." ||
-	       echo "[${ITERATOR}] ERROR: BATSIM finished forcefully." && exit &
+	       echo "[${NAME}] Batsim finished successfuly." ||
+	       echo "[${NAME}] ERROR: BATSIM failed to execute." &
+	BATSIM_PID=${!}
 
         batsched -v "${VARIANT}" \
                  --variant_options_filepath="${FULL_VOPTS}" \
                  --socket-endpoint="ipc://socket_${NAME}" &> "${OUTPUT_DIR}/batsched_${NAME}" &&
-	         echo "[${ITERATOR}] Batsched finished successfuly." ||
-	         echo "[${ITERATOR}] ERROR: BATSCHED finished forcefully." &
+	         echo "[${NAME}] Batsched finished successfuly." ||
+	         echo "[${NAME}] ERROR: BATSCHED failed to execute." &
+	BATSCHED_PID=${!}
 	
-        wait
+	wait %1 || echo "ERROR: Experiment ${NAME} failed or timed out." && kill %2 && kill %1
     ) &
     
 done < <(eval "python3 parse_toml.py ${TOML_FILE}")
